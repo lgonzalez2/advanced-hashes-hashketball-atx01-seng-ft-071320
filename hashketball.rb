297339615419248de2048ae21aaf235c1d3bc873
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-08 02:23:05 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
-# Write your code below game_hash
+require "pry"
+
+
 def game_hash
   {
     home: {
@@ -126,4 +128,161 @@ def game_hash
   }
 end
 
-# Write code here
+
+
+
+
+
+
+def num_points_scored(name)
+  points_scored = 0 
+  
+  game_hash.each do |team, team_attributes|
+    team_attributes.each do |attribute, value|
+      if attribute == :players
+        value.each do |hash_element| 
+          if name == hash_element[:player_name]
+            points_scored = hash_element[:points]
+          end
+        end 
+     end 
+    end 
+  end 
+  points_scored
+end 
+
+
+
+
+
+
+
+def shoe_size(name)
+  shoe_size = 0 
+  
+  game_hash.each do |team, team_attributes|
+    team_attributes.each do |attribute, value|
+      if attribute == :players
+        value.each do |hash_element| 
+          if name == hash_element[:player_name]
+            shoe_size = hash_element[:shoe]
+          end
+        end 
+     end 
+    end 
+  end 
+  shoe_size
+end
+
+
+
+
+
+
+
+def team_colors(team)
+   game_hash.each do |key, value|
+    value.each do |in_key, in_value|
+      if in_value == team
+        return game_hash[key][:colors]
+      end 
+    end
+  end 
+end
+
+
+
+
+
+
+def team_names
+  team_names_array = []
+  
+  game_hash.each do |key, value|
+    value.each do |in_key, in_value|
+      if value[in_key] == in_value && team_names_array.include?(game_hash[key][:team_name]) == false
+        
+        team_names_array << game_hash[key][:team_name]
+      end 
+    end
+  end
+  team_names_array
+end
+
+
+
+
+
+
+def player_numbers(team)
+  numbers = []
+  count = 0 
+  
+ game_hash.each do |key, value|
+    value.each do |in_key, in_value|
+      if in_value == team
+        while count < game_hash[key][:players].length do 
+          numbers << game_hash[key][:players][count][:number]
+          count += 1 
+        end 
+      end 
+    end
+  end 
+  
+  numbers
+end
+
+
+
+
+
+
+
+def player_stats(name)
+  game_hash.each do |team, team_attributes|
+    team_attributes.each do |attribute, value|
+      if attribute == :players
+        value.each do |hash_element| 
+          if name == hash_element[:player_name]
+            return hash_element
+          end
+        end 
+     end 
+    end 
+  end 
+end
+
+
+
+
+
+
+
+
+def big_shoe_rebounds
+  shoe_size = 0 
+  rebound_num = 0
+  
+  game_hash.each do |team, team_attributes|
+    team_attributes.each do |attribute, value|
+      if attribute == :players
+        value.each do |hash_element| 
+          if hash_element[:shoe] > shoe_size
+            shoe_size = hash_element[:shoe]
+          end
+        end 
+        
+        value.each do |hash_element|
+          if shoe_size == hash_element[:shoe]
+            rebound_num = hash_element[:rebounds]
+          end 
+        end 
+     end 
+    end 
+  end 
+  rebound_num
+end
+
+
+
+big_shoe_rebounds
