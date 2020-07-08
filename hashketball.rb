ab9cfa161b4403a3837f25f3d4d2effeb1b8f483
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-08 01:45:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -253,6 +253,7 @@ def big_shoe_rebounds
         element.each do |in_key2, in_value2|
           if game_hash[key][in_key][element][:shoe] > shoe_size
             shoe_size = game_hash[key][in_key][element][:shoe] 
+          end
         end 
       end 
     end 
